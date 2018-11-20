16645f2214a3b63d654f39b1aa2a4636762dc417
atsvetkova <atsvetkova7@gmail.com>
2018-11-20 15:15:22 +0200

    router: user, register, login. validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://nastya:a!1510!a@ds063158.mlab.com:63158/devconnector'
+    mongoURI: 'mongodb://nastya:a!1510!a@ds063158.mlab.com:63158/devconnector',
+    secretOrKey: 'secret'
 }
