8848278b4ac1718b8b1f168bfca9fc96a62ac511
atsvetkova <atsvetkova7@gmail.com>
2018-11-23 16:18:55 +0200

    add dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authActions';
-import { clearCurrentProfile } from './actions/profileActions';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 import { Provider } from "react-redux";
-import store from './store';
+import store from "./store";
 
-import PrivateRoute from './components/common/PrivateRoute';
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -15,11 +15,15 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import "./App.css";
 
 // Check for token
-if(localStorage.jwtToken) {
+if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
 
   const decoded = jwt_decode(localStorage.jwtToken);
@@ -33,7 +37,7 @@ if(localStorage.jwtToken) {
     // Clear current Profile
     store.dispatch(clearCurrentProfile());
     // Redirect to login
-    window.location.href = '/login';
+    window.location.href = "/login";
   }
 }
 
@@ -50,6 +54,34 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
               </Switch>
             </div>
             <Footer />
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_ERRORS, GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -20,6 +20,84 @@ export const getCurrentProfile = () => dispatch => {
         )
 }
 
+// Create profile
+export const createProfile = (profileDate, history) => dispatch => {
+    axios
+        .post('/api/profile', profileDate)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+         }))
+}
+
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+         }))
+}
+
+// Delete experience
+export const deleteExperience = (id) => dispatch => {
+    axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res =>  dispatch({ 
+            type: GET_PROFILE,
+            payload: res.data
+         }))
+        .catch(err => dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+         }))
+}
+
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post('/api/profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+         }))
+}
+
+// Delete education
+export const deleteEducation= (id) => dispatch => {
+    axios
+        .delete(`/api/profile/education/${id}`)
+        .then(res =>  dispatch({ 
+            type: GET_PROFILE,
+            payload: res.data
+         }))
+        .catch(err => dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+         }))
+}
+
+// Delete
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res => dispatch({ 
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            )
+            .catch(err => dispatch({ 
+                type: GET_ERRORS,
+                payload: err.response.data
+            }))
+    }
+}
+
 export const setProfileLoading = () => {
     return {
         type: PROFILE_LOADING
@@ -32,3 +110,4 @@ export const clearCurrentProfile = () => {
         type: CLEAR_CURRENT_PROFILE
     }
 }
+
